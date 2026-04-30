8b989eca97a233c9658cd240db8dd3099cb2e439
Isacc Campos Huanca <isacccampos100@gmail.com>
2026-04-29 20:05:18 -0500

    Deposito

--- Program.cs
+++ Program.cs
@@ -1,1 +1,32 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System.Diagnostics;
+using System.Net;
+using System.Runtime.ConstrainedExecution;
+
+double saldo = 1000;
+int alt = 0;
+while (alt != 4)
+{
+    System.Console.WriteLine("------BIENVENIDO AL SISTEMA BANCARIO------" );
+    System.Console.WriteLine("-----LE BRINDAMOS EL MENU DE OPCIONES-----");
+    System.Console.WriteLine("1.- Consulta tu saldo ");
+    System.Console.WriteLine("2.- Deposita tu dinero aquí ");
+    System.Console.WriteLine("3.- Retira tu dinero aquí ");
+    System.Console.WriteLine("4.- Salir ");
+    System.Console.Write("Que opción desea seleccionar: " );
+    alt = int.Parse(Console.ReadLine()!);
+    switch (alt)
+    {
+        case 1:
+        Console.WriteLine("Este es tu saldo actual:$ " + saldo);
+        break;
+        case 2: 
+        System.Console.WriteLine("Cuanto dinero desea depositar?: ");
+        double deposito = double.Parse(Console.ReadLine()!);
+        saldo = saldo + deposito ;
+        Console.WriteLine("Deposito hecho con éxito.");
+        Console.WriteLine("Ahora tu nuevo saldo es de :" + saldo);
+        break;
+        
+    }
+}
+
--- obj/Debug/net10.0/EJERCICIO DE T1.AssemblyInfo.cs
+++ obj/Debug/net10.0/EJERCICIO DE T1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EJERCICIO DE T1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+119b030046dc07132409adb442f9a867df7010ff")]
 [assembly: System.Reflection.AssemblyProductAttribute("EJERCICIO DE T1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EJERCICIO DE T1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
